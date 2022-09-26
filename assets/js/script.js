d166afa9ad0d314b44f9edd501f737bcb14101ff
junjunjunadi16 <junjunjunadi16@gmail.com>
2022-09-27 06:50:57 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,3 +24,26 @@ window.onscroll = function(){
 }
 
 
+// blob js start
+var radius = 8;
+TweenMax.staggerFromTo('.blob', 4, {
+cycle: {
+  attr: function (i) {
+    var r = i * 90;
+    return {
+      transform: 'rotate(' + r + ') translate(' + radius + ',0.1) rotate(' + (-r) + ')'
+    }
+  }
+}
+}, {
+cycle: {
+  attr: function (i) {
+    var r = i * 90 + 360;
+    return {
+      transform: 'rotate(' + r + ') translate(' + radius + ',0.1) rotate(' + (-r) + ')'
+    }
+  }
+},
+ease: Linear.easeNone,
+repeat: -1
+});
